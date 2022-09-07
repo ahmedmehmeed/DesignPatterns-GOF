495aa4d347cb93b3ba4b04cb4e8f0ce5917295c0
Ahmed Ali <ahmed.abdullah.mehmed@gmail.com>
2022-09-07 23:44:36 +0200

    Abstract Factory

---
+++ Creational-Patterns/AbstractFactory/Program.cs
@@ -0,0 +1,117 @@
+﻿
+/// <summary>
+/// Abstract Factory
+/// </summary>
+public interface IShoppingCartFactory
+{
+    IDiscountService CreateDiscount();
+    IShoppingCostService CreateShoppingCost();
+
+}
+
+/// <summary>
+/// Abstract Product
+/// </summary>
+public interface IDiscountService
+{
+    int DiscountPercentage { get; }
+}
+
+/// <summary>
+/// Abstract Product
+/// </summary>
+public interface IShoppingCostService
+{
+    int ShoppingCost { get; }
+
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class RecentClothesDiscount : IDiscountService
+{
+    public int DiscountPercentage => 20;
+}
+
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class RecentFoodsDiscount : IDiscountService
+{
+    public int DiscountPercentage => 10;
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class AlexShopingCostService : IShoppingCostService
+{
+    public int ShoppingCost => 500;
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class CairoShopingCostService : IShoppingCostService
+{
+    public int ShoppingCost => 100;
+}
+
+/// <summary>
+/// 
+/// Concrete Factory
+/// </summary>
+public class CairoShoppingCartFactory : IShoppingCartFactory
+{
+    public IDiscountService CreateDiscount()
+    {
+        return new RecentClothesDiscount();
+    }
+
+    public IShoppingCostService CreateShoppingCost()
+    {
+        return new CairoShopingCostService();
+    }
+}
+
+/// <summary>
+/// 
+/// Concrete Factory
+/// </summary>
+public class  AlexShoppingCartFactory : IShoppingCartFactory
+{
+    public IDiscountService CreateDiscount()
+    {
+        return new RecentClothesDiscount();
+    }
+
+    public IShoppingCostService CreateShoppingCost()
+    {
+        return new AlexShopingCostService();
+    }
+}
+
+/// <summary>
+/// Client Cart 
+/// </summary>
+public class Cart
+{
+    private readonly IDiscountService discountPercentage;
+    private readonly IShoppingCostService ShoppingCost;
+    private readonly int Itemcost;
+    public Cart(IShoppingCartFactory factory)
+    {
+        discountPercentage = factory.CreateDiscount();
+        ShoppingCost = factory.CreateShoppingCost();
+        Itemcost = 200;
+    }
+
+    public void CalCulateCost()
+    {
+    
+        Console.WriteLine($"Total Cost = "+$"{Itemcost-(Itemcost/100* discountPercentage.DiscountPercentage)+ShoppingCost.ShoppingCost}");
+    }
+
+}
---
+++ Creational-Patterns/AbstractFactory/Test.cs
@@ -0,0 +1,12 @@
+﻿
+
+var AlexShoppingCartFactory = new AlexShoppingCartFactory();
+var AlexShoping = new Cart(AlexShoppingCartFactory);
+AlexShoping.CalCulateCost();
+
+
+var CairoShoppingCartFactory = new CairoShoppingCartFactory();
+var CairoShoping = new Cart(CairoShoppingCartFactory);
+CairoShoping.CalCulateCost();
+
+Console.ReadLine();
